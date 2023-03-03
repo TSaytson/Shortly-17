6b5198ab9a9d7b8ad1a35721cb2ba613b71fe5a0
Thiago Saytson <thiagosaytson@poli.ufrj.br>
2023-03-03 09:40:23 -0300

    dump

--- dump.sql
+++ dump.sql
@@ -28,7 +28,7 @@ CREATE TABLE public.sessions (
     id integer NOT NULL,
     token text NOT NULL,
     "userId" integer NOT NULL,
-    "createdAt" date DEFAULT now()
+    "createdAt" timestamp without time zone DEFAULT now()
 );
 
 
@@ -61,7 +61,7 @@ CREATE TABLE public.users (
     name character varying(100) NOT NULL,
     email character varying(100) NOT NULL,
     password text NOT NULL,
-    "createdAt" date DEFAULT now()
+    "createdAt" timestamp without time zone DEFAULT now()
 );
 
 
@@ -103,30 +103,26 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 -- Data for Name: sessions; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.sessions VALUES (1, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOiJ0aGlhZ29AZy5jb20iLCJpYXQiOjE2Nzc4NDUzNDgsImV4cCI6MTY4MDQzNzM0OH0.fhBvQXknJc6lwFjQbwCB60YivP_1-MpI64O6su8rbnI', 2, '2023-03-03');
-INSERT INTO public.sessions VALUES (2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJtYXJpc2x2Y0BnLmNvbSIsImlhdCI6MTY3Nzg0NTM5NiwiZXhwIjoxNjgwNDM3Mzk2fQ.IOW-KwRU5g5K_towI-0NMexBynH2vnZc9ZXd7la3rE8', 1, '2023-03-03');
 
 
 --
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.users VALUES (1, 'Mariana', 'marislvc@g.com', '$2b$10$aoTArFBEl5tX2OD57vjh8.smJibqZTec4i.BkRsqkGbY9kObpL/v2', '2023-03-03');
-INSERT INTO public.users VALUES (2, 'Thiago', 'thiago@g.com', '$2b$10$xLcPLfddlbjw28y8HDWxcumP4zn7piNPQ5GbeqGzLeKN5Jx8SpvgS', '2023-03-03');
 
 
 --
 -- Name: sessions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.sessions_id_seq', 2, true);
+SELECT pg_catalog.setval('public.sessions_id_seq', 1, false);
 
 
 --
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 2, true);
+SELECT pg_catalog.setval('public.users_id_seq', 1, false);
 
 
 --
